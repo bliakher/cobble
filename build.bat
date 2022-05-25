deea09fbb436aaac454b858f13aaf02cc9e2932f
Evgenia Golubeva <44325210+bliakher@users.noreply.github.com>
2022-05-25 09:44:56 +0200

    change target to release, copy files

--- build.bat
+++ build.bat
@@ -1,4 +1,5 @@
 
 cmake cobble_src -B build
-cmake --build build
-copy SDL2\lib\x64\*.dll build\Debug\
+cmake --build build --config Release
+copy SDL2\lib\x64\*.dll build\Release\
+xcopy cobble_src\data build\Release\data\ /E
